e7b70df557b63adcfaeb8ebab5b42596a9210669
robbergen <38932016+robbergen@users.noreply.github.com>
2023-05-16 09:41:30 -0700

    Update hecktor_dataset.py
    Fixed a bug for unconditional GAN

--- datasets/hecktor_dataset.py
+++ datasets/hecktor_dataset.py
@@ -7,7 +7,8 @@ class HecktorDataset(chainer.dataset.DatasetMixin):
     def __init__(self, n_frames, dataset_path):
         self.dset = np.load(dataset_path)
         if np.ndim(self.dset)<5: #If there is no conditional mask
-            elf.dset = self.dset.transpose([1, 0, 2, 3])
+            self.dset = self.dset.transpose([1, 0, 2, 3])
+            self.dset = self.dset[:,:,np.newaxis,:,:]
         else:
             self.dset = self.dset.transpose([1, 0, 4, 2, 3]) #If there is a conditional mask there is no need for a new axis
         self.n_frames = n_frames
